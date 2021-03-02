eb4ce6555e71af853881ce07665d1613708448da
ssruoh <ssruoh@gmail.com>
2021-03-02 00:03:36 +0000

    urls, templates, migrations & admin, models, creating data

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
-def say_hello(request):
-    return HttpResponse("Hello!")
-
-
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
